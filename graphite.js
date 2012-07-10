5fc0c2c0ce68e3063cd753e4bb23cb592f170d6d
Lonnen <chris.lonnen@gmail.com>
2012-07-10 16:14:12 -0700

    moar closures, completed parameters list

--- graphite.js
+++ graphite.js
@@ -1,83 +1,189 @@
-var Graphite = (function() {
-    var g, ValueError,
-        config = {
-            host: "/render",
-        },
-        parameter_list = [
-             "targets",
-             "from",
-             "until",
-             "format",
-             //"rawData", //deprecated
-             "areaAlpha",
-             "areaMode",
-             "bgcolor",
-             "cacheTimeout",
-             "colorList"
-        ];
+(function(win) {
+    Graphite = (function() {
+        return function() {
+            var config = {
+                    host: "/render",
+                },
+                parameter_list = [
+                     "host",
+                     "targets",
+                     "from",
+                     "until",
+                     "format",
+                     //"rawData", //deprecated
+                     "areaAlpha",
+                     "areaMode",
+                     "bgcolor",
+                     "cacheTimeout",
+                     "colorList",
+                     "drawNullAsZero",
+                     "fgcolor",
+                     "fontBold",
+                     "fontItalic",
+                     "fontName",
+                     "fontSize",
+                     "format",
+                     "from",
+                     "graphOnly",
+                     "graphTypes",
+                     "hideLegend",
+                     "hideAxes",
+                     "hideYAxis",
+                     "hideGrid",
+                     "height",
+                     "jsonp",
+                     "leftColor",
+                     "leftDashed",
+                     "leftWidth",
+                     "lineMode",
+                     "lineWidth",
+                     "logBase",
+                     "localOnly",
+                     "majorGridLineColor",
+                     "margin",
+                     "max",
+                     "minorGridLineColor",
+                     "minorY",
+                     "min",
+                     "minXStep",
+                     "noCache",
+                     "pickle",
+                     "pieMode",
+                     "rightColor",
+                     "rightDashed",
+                     "rightWidth",
+                     "template",
+                     "thickness",
+                     "title",
+                     "tz",
+                     "uniqueLegend",
+                     "until",
+                     "vtitle",
+                     "vtitleRight",
+                     "width",
+                     "xFormat",
+                     "yAxisSide",
+                     "yLimit",
+                     "yLimitLeft",
+                     "yLimitRight",
+                     "yMin",
+                     "yMax",
+                     "yMaxLeft",
+                     "yMaxRight",
+                     "yMinLeft",
+                     "yMinRight",
+                     "yStep",
+                     "yStepLeft",
+                     "yStepRight",
+                     "yUnitSystem",
+                ];
 
-    /* Public: functor providing an interface to the internal closure state
-     *
-     *
-     * initialConfig    - an object describing initial parameters and values
-     *                    (default: {})
-     *
-     * Examples
-     *
-     *  Graphite()
-     *
-     *  Graphite({from: "-24hours"})
-     *
-     * Returns itself so that methods can be chained
-     */
-    function g(initialConfig) {
-        if (!arguments.length) {
-            return g;
-        }
+            /* Public: functor providing an interface to the internal closure state
+             *
+             * initialConfig    - an object describing initial parameters and values
+             *                    (default: {})
+             *
+             * Examples
+             *
+             *  Graphite()
+             *
+             *  Graphite({from: "-24hours"})
+             *
+             * Returns itself so that methods can be chained
+             */
+            function g(initialConfig) {
+                if (!arguments.length) {
+                    return g;
+                }
 
-        for (var i in initialConfig) {
-            config[i] = initialConfig[i];
-        }
+                for (var i in initialConfig) {
+                    config[i] = initialConfig[i];
+                }
 
-        return g;
-    }
-
-    /* Public: Get or set request parameters. When called without arguments it
-     *         acts as a getter and returns the parameter's value or undefined.
-     *         When called with arguments, the 0th argument is set as the
-     *         parameter's value and the functor is returned for chainability.
-     *
-     * value    - the value to be set for the parameter (optional).
-     *
-     * Examples
-     *
-     *   Graphite().target(["some.key"])
-     *
-     * Returns the functor object for further chaining.
-     *
-     *   Graphite().from("-24hours").from()
-     *
-     * Returns "-24hours", the value of the from parameter.
-     *
-     * Signature
-     *
-     *  <parameter>([value])
-     *
-     * parameter - the query parameter to set. see the parameter_list variable
-     *             enumeration of all the legal parameters. see the graphite
-     *             render api doc for param defaults
-     */
-    parameter_list.map(function(prop) {
-        g[prop] = function(value) {
-            var j;
-            if (!arguments.length) {
-                return config[prop];
+                return g;
             }
 
-            config[prop] = value;
+            /* Public: Get or set request parameters. When called without arguments it
+             *         acts as a getter and returns the parameter's value or undefined.
+             *         When called with arguments, the 0th argument is set as the
+             *         parameter's value and the functor is returned for chainability.
+             *
+             * value    - the value to be set for the parameter (optional).
+             *
+             * Examples
+             *
+             *   Graphite().target(["some.key"])
+             *
+             * Returns the functor object for further chaining.
+             *
+             *   Graphite().from("-24hours").from()
+             *
+             * Returns "-24hours", the value of the from parameter.
+             *
+             * Signature
+             *
+             *  <parameter>([value])
+             *
+             * parameter - the query parameter to set. see the parameter_list variable
+             *             enumeration of all the legal parameters. see the graphite
+             *             render api doc for param defaults
+             */
+            parameter_list.map(function(prop) {
+                g[prop] = function(value) {
+                    var j;
+                    if (!arguments.length) {
+                        return config[prop];
+                    }
+
+                    config[prop] = value;
+                    return g;
+                };
+            });
+
+            /* Public: Compile a url from the current internal state.
+             *
+             * Examples
+             *
+             *   Graphite().url();
+             *
+             * Returns the string "/render"
+             *
+             *   Graphite({ from: "-2days" }).targets(['some.key']).url();
+             *
+             * Returns the string "/render?from=-2days&target=some.key"
+             *
+             */
+            g.url = function() {
+                var url = config['host'] + "?",
+                    properties = [],
+                    prop, i;
+                // to get a consistent ordering for url properties, extract all the
+                // property names, sort them, and then construct the url. useful for
+                // testing.
+                for (var prop in config) {
+                    if (prop === "host") {
+                        continue;
+                    }
+                    properties.push(prop);
+                }
+                properties.sort();
+                for (i = 0; i < properties.length; i++) {
+                    prop = properties[i];
+                    if (prop === "targets") {
+                        targets = config[prop];
+                        for (j = 0; j < targets.length; j++) {
+                            url += "target=" + targets[j] + "&";
+                        }
+                        continue;
+                    }
+                    url += prop + "=" + config[prop] + "&";
+                }
+                // pretty the url for the people
+                return url.substring(0, url.length - 1);
+            }
+
             return g;
         };
-    });
-
-    return g;
-})();
+    })();
+    win.Graphite = Graphite;
+})(window);
--- tests/tests.js
+++ tests/tests.js
@@ -1,33 +1,33 @@
-test("noop", function() {
+test("noop", 1, function() {
     var url = Graphite().url();
-    equal(url, "/render?");
+    equal(url, "/render");
 });
 
-test("minimal functional example", function() {
-    var url = Graphite().target(["some.key"]).url();
+test("minimal functional example", 1, function() {
+    var url = Graphite().targets(["some.key"]).url();
     equal(url, "/render?target=some.key");
 });
 
-test("multiple targets requre an array of target keys", function() {
-    var url = Graphite.target(["some.key", "another.key"]).url();
+test("multiple targets requre an array of target keys", 1, function() {
+    var url = Graphite().targets(["some.key", "another.key"]).url();
     equal(url, "/render?target=some.key&target=another.key");
 });
 
-test("parameters can be initialized in constructor", function() {
+test("parameters can be initialized in constructor", 1, function() {
     var url = Graphite({
         from: "-2days",
         until: "now",
         target: ["some.key"],
     }).url();
-    equal(url, "/render?target=some.key&from=-2days&until=now");
+    equal(url, "/render?from=-2days&target=some.key&until=now");
 });
 
-test("parameters can be accumulated through chaning", function() {
-    var url = Graphite().from("-2days").until("now").target(["some.key"]);
-    equal(url, "/render?target=some.key&from=-2days&until=now");
+test("parameters can be accumulated through chaning", 1, function() {
+    var url = Graphite().from("-2days").targets(["some.key"]).url();
+    equal(url, "/render?from=-2days&target=some.key");
 });
 
-test("properties use getter-setter methods", function() {
+test("properties use getter-setter methods", 2, function() {
     var g = Graphite(),
         oldHost,
         newHost;
@@ -38,14 +38,14 @@ test("properties use getter-setter methods", function() {
     equal(newHost, "http://betrics.biz/render");
 });
 
-test("allows non-string values", function() {
-    var url = Graphite().height(300);
+test("allows non-string values", 1, function() {
+    var url = Graphite().height(300).url();
     equal(url, "/render?height=300");
 });
 
-test("allows ludicrous values", function() {
-    var url = Graphite().graphType("lolwut?");
-    equal(url, "/render?graphType=lolwut?");
-    url = Graphite().graphType(1337);
-    equal(url, "/render?graphType=1337");
+test("allows ludicrous values", 2, function() {
+    var url = Graphite().graphTypes("lolwut?").url();
+    equal(url, "/render?graphTypes=lolwut?");
+    url = Graphite().graphTypes(1337).url();
+    equal(url, "/render?graphTypes=1337");
 });
